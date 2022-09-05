336b4ab5b8c91b8abbab66833aa6fdda7f4eb840
methodDen <daniyarabsatov@gmail.com>
2022-09-05 18:34:36 +0600

    added: new comment

--- instant_shop_back/api/admin.py
+++ instant_shop_back/api/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from api.models import Category, Product,Shop,City
 from django.views.decorators.csrf import csrf_exempt
 # Register your models here.
+
+# New comment
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Shop)
